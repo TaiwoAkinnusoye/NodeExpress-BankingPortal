f17dfb4a7f43466f95b35f81eab25717caba43af
Akinnusoye Taiwo <taiwoakinnusoye@gmail.com>
2019-11-05 13:28:11 +0100

    express setup and index route, view setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import express from 'express';
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
 
 const app = express();
 
